115300dc72dbb417fe54b083f5ac1a55a277a4bb
Olivier Heurtier <olivier.heurtier@idemia.com>
2020-03-04 19:52:06 +0100

    Fix tests

--- iso19794/tests/test_iso.py
+++ iso19794/tests/test_iso.py
@@ -12,15 +12,15 @@ class TestFIR(unittest.TestCase):
     def test_fir1(self):
         # check we can read an ISO image
         i = PIL.Image.open(os.path.join(os.path.dirname(__file__),'annexc.fir'))
-        self.assertEqual(i.header.nb_repr,1)
-        self.assertEqual(i.header.nb_pos,1)
-        self.assertEqual(i.header.length,234441)
-        i.rheader = i.rheader._replace(position = 'Right ring finger')
+        self.assertEqual(i.info['nb_representation'],1)
+        self.assertEqual(i.info['nb_position'],1)
+        # self.assertEqual(i.info['length'],234441)
+        i.header = i.header._replace(position = 'Right ring finger')
         self.assertRaises(EOFError,i.seek,1)
         # rotate the image
         i2 = i.rotate(45,expand=True,fillcolor=128)        
         i2.header = i.header
-        i2.rheader = i.rheader
+        i2.header = i.header
         # check we can save it
         buf = io.BytesIO()
         i2.save(buf,"FIR")
@@ -29,15 +29,15 @@ class TestFIR(unittest.TestCase):
     def test_fir2(self):
         # check we can read an ISO image
         i = PIL.Image.open(os.path.join(os.path.dirname(__file__),'twofingers.fir'))
-        self.assertEqual(i.header.nb_repr,2)
-        self.assertEqual(i.header.nb_pos,2)
-        self.assertEqual(i.rheader.position,'Left index finger')
+        self.assertEqual(i.info['nb_representation'],2)
+        self.assertEqual(i.info['nb_position'],2)
+        self.assertEqual(i.header.position,'Left index finger')
         i.rotate(90)
         i.seek(1)
         i.rotate(90)
-        self.assertEqual(i.header.nb_repr,2)
-        self.assertEqual(i.header.nb_pos,2)
-        self.assertEqual(i.rheader.position,'Left middle finger')
+        self.assertEqual(i.info['nb_representation'],2)
+        self.assertEqual(i.info['nb_position'],2)
+        self.assertEqual(i.header.position,'Left middle finger')
         self.assertRaises(EOFError,i.seek,2)
 
     def test_multi(self):
@@ -47,8 +47,8 @@ class TestFIR(unittest.TestCase):
         i1.save(buf,"FIR",save_all=True,append_images=[i2])
 
         i = PIL.Image.open(buf)
-        self.assertEqual(i.header.nb_repr,3)
-        self.assertEqual(i.header.nb_pos,2)
+        self.assertEqual(i.info['nb_representation'],3)
+        self.assertEqual(i.info['nb_position'],2)
 
 # ______________________________________________________________________________
 if __name__=='__main__':
--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setuptools.setup(
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: CeCILL-C Free Software License Agreement (CECILL-C)",
         "Operating System :: OS Independent",
     ],
